e369084a7bc52b278c2e2db1f4c29abb20306de6
Rohit Patil <rahulhimesh09@gmail.com>
2020-04-24 23:02:43 +0530

    modified so it can embed tags and image for main program

--- tag_embedder.py
+++ tag_embedder.py
@@ -1,6 +1,6 @@
 import eyed3
 
-def tag_embed(file,title,artists,album,album_artists,release_date,track_number,total_tracks):
+def tag_embed(file,title,artists,album,album_artists,release_date,track_number,total_tracks, img_path):
     audio_file = eyed3.load(str(file))
     audio_file.tag.title = title
     audio_file.tag.artist = artists.strip("[]").strip("\'\"")
@@ -8,6 +8,6 @@ def tag_embed(file,title,artists,album,album_artists,release_date,track_number,t
     audio_file.tag.track_num = (track_number, total_tracks)
     audio_file.tag.album_artist = album_artists.strip("[]").strip("\'\"")
     audio_file.tag.release_date = release_date
-    audio_file.tag.images.set(type_=3, img_data=open("./images/img.jpg", 'rb').read(), mime_type="image/jpeg", description=u"album cover", img_url=None)
+    audio_file.tag.images.set(type_=3, img_data=open(img_path, 'rb').read(), mime_type="image/jpeg", description=u"album cover", img_url=None)
     audio_file.tag.save()
-    print(file, title ,artists, album, album_artists, release_date, track_number, total_tracks)
+    print(file, title ,artists, album, album_artists, release_date, track_number, total_tracks, img_path)
