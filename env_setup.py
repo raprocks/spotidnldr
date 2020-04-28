c08041cda95cfe03d5e632baf185d9fc3933c772
Rohit Patil <rahulhimesh09@gmail.com>
2020-04-28 21:26:35 +0530

    :))

--- env_setup.py
+++ env_setup.py
@@ -1,5 +1,5 @@
-SPOTIPY_CLIENT_ID="7983835e2ad84e8097efc6a6c52162b4"
-SPOTIPY_CLIENT_SECRET="c50c167cbf854a79b3013f75fa3fe912"
-SPOTIFY_USER_ID="<your user id>"
+SPOTIPY_CLIENT_ID="<your spotify client id here>"#7983835e2ad84e8097efc6a6c52162b4 you can use this but same message as for the below one
+SPOTIPY_CLIENT_SECRET="<your spotify client secret here>"#c50c167cbf854a79b3013f75fa3fe912  you can use this one but this may be restricted by spotify so try getting yours first.
+SPOTIFY_USER_ID="<your spotify user id here>"
 SPOTIPY_REDIRECT_URI="https://google.com"
-YOUTUBE_API_KEY="<your youtube api key>"
+YOUTUBE_API_KEY="<your youtube api key>"#cant provide one for you cauze youtube restricts searches from one api key to 45-50 per day so you should get one yourself. Sorry.
