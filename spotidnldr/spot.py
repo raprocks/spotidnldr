fde6616fe854c8d145d827609b2412f3c1e7dca6
Rohit Patil <rahulhimesh09@gmail.com>
2020-04-29 22:14:29 +0530

    removed useless imports and the main program has been relocated to clifi.py

--- spotidnldr/spot.py
+++ spotidnldr/spot.py
@@ -1,14 +1,5 @@
 import spotipy
 import spotipy.util as util
-import json
-import os
-from spotidnldr.cover_download import dl_jpg
-from spotidnldr.youtube_search import youtube
-from spotidnldr.tag_embedder import tag_embed
-import spotidnldr.downloader
-from spotidnldr.converter import convert_to_mp3
-from spotidnldr.env_setup import *
-from spotidnldr.env_checker import *
 
 class spotr:
     def __init__(self, userid=SPOTIFY_USER_ID):
@@ -122,29 +113,4 @@ class spotr:
         return return_dict
 
 if __name__=="__main__":
-    local=locals()
-    check_for_env_vars(a=local, env=os.environ)
-    res = spotr().get_song_info(input("input url to dnld🙏🙏 >>> "))
-    #print(json.dumps(res, indent=3))
-    #print(len(res))
-    for each in res:
-        os.system("youtube-dl --rm-cache-dir")
-        album_cover_url = each["album_data"]["album_cover"]
-        name = each["track_data"]["name"]+ " - " + str(each["track_data"]["artists"]).strip("[]").strip("\'").replace("\'", "")
-        title = each["track_data"]["name"]
-        song_artists = str(each["track_data"]["artists"]).strip("[]").strip("\'").replace("\'", "")
-        album_name = each["album_data"]["name"]
-        album_artists = str(each["album_data"]["album_artists"]).strip("[]").strip("\'").replace("\'", "")
-        track_number = each["track_data"]["track_number"]
-        total_tracks = each["album_data"]["total_tracks"]
-        release_date = each["album_data"]["album_release_date"]
-        if "/" in name:
-            name = name.replace("/\\","" )
-        img_name = dl_jpg(album_cover_url, "./", name)
-        retrived_from_youtube=youtube().search(query=name,order="relevance", limit=10, return_indices=3)
-        infile = downloader.download(url=retrived_from_youtube[0], file_name=name)
-        convert_to_mp3(infile, f"./{name}.mp3")
-        tag_embed(f"{name}.mp3", title=title, artists=song_artists, album=album_name, album_artists=album_artists, release_date=release_date, track_number=track_number, total_tracks=total_tracks, img_path=img_name)
-        os.remove(img_name)
-        os.remove(infile)
-    print("done")
+    print("this is not the main program this just retrives data from spotify using their web api")
