e2dfcfeff65f7a6bc9656ba3d336f569e7b0b13f
Rohit Patil <rahulhimesh09@gmail.com>
2020-04-29 15:56:36 +0530

    added a script for termux

---
+++ termux_setup.sh
@@ -0,0 +1,31 @@
+#pkg update
+#pkg upgrade -y
+#set storage permissions
+echo "Please allow to provide access to internal storage"
+termux-setup-storage
+#get packages
+echo "Installing needed packages for Termux ...."
+pkg install -y ffmpeg python git micro
+#make bin if not present
+if [ ! -d "$HOME/bin" ]; then
+	mkdir "$HOME/bin"
+fi
+# get gist, put it in termux url opener and set it as executable
+echo 'url' > "$HOME/bin/termux-url-opener.sh"
+chmod +x termux-url-opener.sh
+#make a directory to store songs into
+if [ ! -d "/storage/emulated/0/Songs" ]; then
+	mkdir "/storage/emulated/0/Songs"
+fi
+
+#get spotidnldr
+echo "Getting Spotidnldr"
+git clone https://github.com/raprocks/spotidnldr.git $HOME/spotidnldr
+# traverse into directory
+cd spotidnldr
+#opening api key setup script to edit
+micro env_setup.py
+
+echo " You are good to go now just share your spotify link to termux from spotify and the song will be downloaded to the folder named Songs in Internal Memory"
+
+exit 0
