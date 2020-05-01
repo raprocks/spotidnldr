90353f89de798381db174f2994e21e52a59d839c
Rohit Patil <rahulhimesh09@gmail.com>
2020-05-01 13:04:51 +0530

    dumb me put some dumb path as default so now it is fixed

--- spotidnldr/clifi.py
+++ spotidnldr/clifi.py
@@ -9,7 +9,7 @@ from spotidnldr.tag_embedder import *
 from spotidnldr.converter import *
 
 @click.command()
-@click.option('-o', "--output", help="provide a output path for the song explicitly", default="/storage/emulated/storage/0/Songs/")
+@click.option('-o', "--output", help="provide a output path for the song explicitly", default="/storage/emulated/0/Songs/")
 @click.option("--url", prompt="enter Url of spotify song", help="The flag which directly sets the url instead of a prompt. if not used the program will prompt you for a url", required=True, type=str)
 def download(url,output):
     env_list = ["SPOTIPY_CLIENT_ID", "SPOTIPY_CLIENT_SECRET", "SPOTIFY_USER_ID", "SPOTIPY_REDIRECT_URI", "YOUTUBE_API_KEY"]
@@ -25,10 +25,11 @@ def download(url,output):
             print(f"{var} is set in env_vars.py")
         else:
             print(f"{var} is not set")
-    userid=SPOTIFY_USER_ID
-    clientid=SPOTIPY_CLIENT_ID
-    clientsecret=SPOTIPY_CLIENT_SECRET
-    redirecturi=SPOTIPY_REDIRECT_URI
+    userid=locals()["SPOTIFY_USER_ID"]
+    clientid=locals()["SPOTIPY_CLIENT_ID"]
+    clientsecret=locals()["SPOTIPY_CLIENT_SECRET"]
+    redirecturi=locals()["SPOTIPY_REDIRECT_URI"]
+    youtu_key=locals()["YOUTUBE_API_KEY"]
     res = spotr(userid=userid, clientid=clientid, clientsecret=clientsecret, redirecturi=redirecturi).get_song_info(url)
     for each in res:
         os.system("youtube-dl --rm-cache-dir")
@@ -46,7 +47,7 @@ def download(url,output):
         if ".temp" not in os.listdir():
             os.mkdir(".temp")
         img_name = dl_jpg(album_cover_url, "./.temp/", name)
-        retrived_from_youtube=youtube().search(query=name,order="relevance", limit=10, return_indices=3)
+        retrived_from_youtube=youtube(key=youtu_key).search(query=name,order="relevance", limit=10, return_indices=3)
         infile = download_yout(url=retrived_from_youtube[0], file_name=name)
         convert_to_mp3(infile, os.path.join(output,f"{name}.mp3"))
         tag_embed(os.path.join(output,f"{name}.mp3"), title=title, artists=song_artists, album=album_name, album_artists=album_artists, release_date=release_date, track_number=track_number, total_tracks=total_tracks, img_path=img_name)
