86974d7e4bf4a43032fff1f5e84afb8b920b0334
Rohit Patil <rahulhimesh09@gmail.com>
2020-05-04 17:37:24 +0530

    now no need of youtube api key and no limit of songs

--- spotidnldr/env_setup.py
+++ spotidnldr/env_setup.py
@@ -1,3 +1,4 @@
+#dont change anything here unless you know what you are doing
 SPOTIPY_CLIENT_ID="7983835e2ad84e8097efc6a6c52162b4"
 SPOTIPY_CLIENT_SECRET="c50c167cbf854a79b3013f75fa3fe912"
-YOUTUBE_API_KEY="<your youtube api key>"#cant provide one for you cauze youtube restricts searches from one api key to 45-50 per day so you should get one yourself. Sorry.
+#dont change anything here unless you knwo what you are doing
