76ffdb648b30cf758198bee5833df309948b214e
Rohit Patil <rahulhimesh09@gmail.com>
2020-05-04 17:36:54 +0530

    changed youtube searcher to youtube *music* api now.

--- spotidnldr/youtube_search.py
+++ spotidnldr/youtube_search.py
@@ -1,26 +1,11 @@
+from ytmusicapi import YTMusic
 
-import requests
-import os
-import json
 
-class youtube:
-    def __init__(self,key):
-        self.api_key = key
-        self.base = "https://www.googleapis.com/youtube/v3"#https://www.googleapis.com/youtube/v3/search?part=snippet&q=Retrovision-tommorowland%202019&prettyPrint=true&key=
-    def _url(self, endpoint):
-        return self.base + "/" + str(endpoint)
-    def search(self, query, order, limit, return_indices):
-        self.parameters={"part":"snippet", "q":str(query),"order":str(order),"maxResults":int(limit),"type":"video","videoCategoryId":"10", "key":self.api_key}
-        request_url = str(self._url("search"))
-        self.obje = requests.get(request_url, params=self.parameters)
-        print(self.obje)
-        self.search_results=self.obje.json()["items"]
-        _res = []
-        for x in self.search_results:
-            _res.append(x["id"]["videoId"])
-        print(_res[:return_indices])
-        return [_res[:return_indices]]
-
+def youtube_search(q,return_indices, filters="songs"):
+    res = YTMusic().search(query=q,filter=filters)
+    x = [[video["videoId"]] for video in res]
+    print(x[:return_indices])
+    return x[:return_indices]
 
 if __name__=="__main__":
-    print("this ain't the main program.please use spoti --help")
+    print("this is not the main program use spoti --help")
