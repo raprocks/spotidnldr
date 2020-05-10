5537475a18308741c157402429cab32c3df9ffcd
Rohit Patil <rahulhimesh09@gmail.com>
2020-05-10 13:37:49 +0530

    removed url sharing gist curl

--- termux_setup.sh
+++ termux_setup.sh
@@ -10,15 +10,15 @@ if [ -d "$HOME/spotidnldr" ]; then
 	rm -rf $HOME/spotidnldr
 fi
 #make bin if not present
-if [ ! -d "$HOME/bin" ]; then
-	mkdir "$HOME/bin"
-fi
+#if [ ! -d "$HOME/bin" ]; then
+#	mkdir "$HOME/bin"
+#fi
 
-touch $HOME/bin/termux-url-opener
+#touch $HOME/bin/termux-url-opener
 # get gist, put it in termux url opener and set it as executable
-curl 'https://raw.githubusercontent.com/raprocks/spotidnldr/master/gist' > "$HOME/bin/termux-url-opener"
+#curl 'https://raw.githubusercontent.com/raprocks/spotidnldr/master/gist' > "$HOME/bin/termux-url-opener"
 
-chmod +x $HOME/bin/termux-url-opener
+#chmod +x $HOME/bin/termux-url-opener
 #make a directory to store songs into
 if [ ! -d "/storage/emulated/0/Songs" ]; then
 	mkdir "/storage/emulated/0/Songs"
@@ -32,6 +32,6 @@ cd $HOME/spotidnldr
 
 pip install .
 
-echo " You are good to go now just share your spotify link to termux from spotify and the song will be downloaded to the folder named Songs in Internal Memory OR use ```spoti --help```"
+echo "the Song will be downloaded to the folder named Songs in Internal Memory  ```spoti --help```"
 
 exit 0
