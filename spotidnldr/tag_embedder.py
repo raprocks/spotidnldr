8c048ac063e056fa9f31ae420fe34f8d17f7d75f
Rohit Patil <rahulhimesh09@gmail.com>
2020-05-11 21:47:20 +0530

    removed print statments

--- spotidnldr/tag_embedder.py
+++ spotidnldr/tag_embedder.py
@@ -10,4 +10,4 @@ def tag_embed(file,title,artists,album,album_artists,release_date,track_number,t
     audio_file.tag.release_date = release_date
     audio_file.tag.images.set(type_=3, img_data=open(img_path, 'rb').read(), mime_type="image/jpeg", description=u"album cover", img_url=None)
     audio_file.tag.save()
-    print(file, title ,artists, album, album_artists, release_date, track_number, total_tracks, img_path)
+    #print(file, title ,artists, album, album_artists, release_date, track_number, total_tracks, img_path)
