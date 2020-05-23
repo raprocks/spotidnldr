d80af15bf753f050c7c7d8a18cb31e7f0d0ec9e0
Rohit Patil <rahulhimesh09@gmail.com>
2020-05-23 15:48:34 +0530

    typo fix

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
     classifiers=[
         "Natural Language :: English",
         "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)","
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
     entry_points='''
