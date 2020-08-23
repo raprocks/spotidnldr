bc1a95bdad5f61c40317cb0cc319f86070dd66ca
Rohit Patil <rahulhimesh09@gmail.com>
2020-08-23 15:57:40 +0530

    added progress bar module for showing download status.

---
+++ spotidnldr/progress_bar.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+def download_progress_bar(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    width = 32
+    phases = (' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█')
+    empty_fill = ' '
+    nphases = len(phases)
+    progress = min(1, (total_size - bytes_remaining)/total_size)
+    filled_len = width * progress
+    nfull = int(filled_len)                      # Number of full chars
+    phase = int((filled_len - nfull) * nphases)  # Phase of last char
+    nempty = width - nfull                  # Number of empty chars
+    prog_bar = phases[-1] * nfull
+    current = phases[phase] if phase > 0 else ''
+    empty = empty_fill * max(0, nempty - len(current))
+    line = ''.join([prog_bar, current, empty])
+
+
+
+
+
+    to_fill = (((total_size - bytes_remaining)/1024)/1024)
+    print("\rDownloading.. |", line, f"| {to_fill:.2f}/{total_size/1024/1024:.2f}", end='')
