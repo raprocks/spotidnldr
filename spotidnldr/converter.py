733a6c9afb0c6732c4bfbedf6a8a7734d0741aa4
Rohit Patil <rahulhimesh09@gmail.com>
2020-09-15 13:56:19 +0530

    audio bitrate argument and some output for aid

--- spotidnldr/converter.py
+++ spotidnldr/converter.py
@@ -1,11 +1,18 @@
 import ffmpeg
 
-def convert_to_mp3(file_path, out_file_path, verbose):
-    input=ffmpeg.input(file_path)
-    audio=input.audio
-    out = ffmpeg.output(audio, out_file_path, format="mp3", audio_bitrate=192000)
-    ffmpeg.run(out, quiet=verbose)
 
-if __name__=="__main__":
+def convert_to_mp3(file_path, out_file_path, audiobitrate=192000, verbose=False):
+    print("[*] converting to mp3")
+    input_file = ffmpeg.input(file_path)
+    audio = input_file.audio
+    out = ffmpeg.output(
+        audio,
+        out_file_path,
+        format="mp3",
+        audio_bitrate=audiobitrate)
+    ffmpeg.run(out, quiet=(not verbose))
+    print("[*] converted to mp3 with bitrate of {audiobitrate/1000}K")
+
+
+if __name__ == "__main__":
     print("main")
-    convert_to_mp3("./hello.webm","./out.mp3")
