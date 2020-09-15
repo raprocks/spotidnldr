fd71087dd7b67a1ed1158b2ae649afec07a3b6ee
Rohit Patil <rahulhimesh09@gmail.com>
2020-09-15 20:07:05 +0530

    code refactor and output statements

--- spotidnldr/cover_download.py
+++ spotidnldr/cover_download.py
@@ -2,21 +2,22 @@ import urllib.request
 import os
 from pathlib import Path
 
+
 def dl_jpg(url, file_path, file_name):
     name = file_name
     #print(url, file_path, name)
     if "/" in name:
-        file_name = name.replace("/","|")
-    elif "\\" in name:
+        file_name = name.replace("/", "|")
+    if "\\" in name:
         file_name = name.replace("\\", "")
-    else:
-        pass
-        
-    full_path = Path(os.path.join(file_path,str(file_name + '.jpg')))
+    print('[*] downloading cover art...', end='')
+    full_path = Path(os.path.join(file_path, str(file_name + '.jpg')))
     urllib.request.urlretrieve(url, full_path)
+    print('Done')
     return full_path
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     url = input('Enter image URL to download: ')
     file_name = input('Enter file name to save as: ')
     if "images" not in os.listdir():
