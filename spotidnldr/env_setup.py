7e3de658fc63254673c3e30de1f54287e6bfc7d8
Rohit Patil <rahulhimesh09@gmail.com>
2020-09-15 20:07:55 +0530

    dont know

--- spotidnldr/env_setup.py
+++ spotidnldr/env_setup.py
@@ -1,4 +1,4 @@
-#dont change anything here unless you know what you are doing
-SPOTIPY_CLIENT_ID="7983835e2ad84e8097efc6a6c52162b4"
-SPOTIPY_CLIENT_SECRET="c50c167cbf854a79b3013f75fa3fe912"
-#dont change anything here unless you knwo what you are doing
+# dont change anything here unless you know what you are doing
+SPOTIPY_CLIENT_ID = "7983835e2ad84e8097efc6a6c52162b4"
+SPOTIPY_CLIENT_SECRET = "c50c167cbf854a79b3013f75fa3fe912"
+# dont change anything here unless you knwo what you are doing
